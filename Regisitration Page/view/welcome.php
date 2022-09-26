bc2254c18f3d1243c2578bb6bb16536b8ad02bbd
itsmaimai <21103807@usc.edu.ph>
2022-09-26 16:40:14 +0800

    add glitch animation

--- Regisitration Page/view/welcome.php
+++ Regisitration Page/view/welcome.php
@@ -27,36 +27,18 @@
 
             <h1 class="title">Hello! Welcome!</h1>
             <?php
-<<<<<<< HEAD
             if (isset($_POST["submit"])) {
-
-                $fname = $_POST["fname"];
-                $sname = $_POST["sname"];
-                $email = $_POST["email"];
-                $address = $_POST["address"];
+                $fname    = $_POST["fname"];
+                $sname    = $_POST["sname"];
+                $email    = $_POST["email"];
+                $address  = $_POST["address"];
                 $password = $_POST["password"];
-                echo "<h3 id='typedtext'>Your_Name: $fname  $sname</h3>";
-                echo "<h3>Your_Address:  $address</h3>";
-                echo "<h3>Your_Email-Address: $email</h3>";
-                echo "<h3>Your_password is $password</h3>";
-                echo "<script></script>";
-=======
 
-
-
-
-if (isset($_POST["submit"])) {
-    $fname    = $_POST["fname"];
-    $sname    = $_POST["sname"];
-    $email    = $_POST["email"];
-    $address  = $_POST["address"];
-    $password = $_POST["password"];
-    
-    echo "<h3 id='typedtext1'>Your_Name: $fname $sname</h3>";
-    echo "<h3 id='typedtext2'>Your_Address:  $address</h3>";
-    echo "<h3 id='typedtext3'>Your_Email-Address: $email</h3>";
-    echo "<h3 id='typedtext4'>Your_password is $password</h3>";
-    echo "<script>
+                echo "<h3 id='typedtext1'>Your_Name: $fname $sname</h3>";
+                echo "<h3 id='typedtext2'>Your_Address:  $address</h3>";
+                echo "<h3 id='typedtext3'>Your_Email-Address: $email</h3>";
+                echo "<h3 id='typedtext4'>Your_password is $password</h3>";
+                echo "<script>
 
             function typeEffect(element, speed) {
                 var text = element.innerHTML;
@@ -71,7 +53,6 @@ if (isset($_POST["submit"])) {
                   clearInterval(timer);
                 }
               }, speed);
->>>>>>> 3e4ec41fa911fcfa2db45b21753a031452e6f319
             }
             
             
@@ -88,10 +69,8 @@ if (isset($_POST["submit"])) {
             typeEffect(txt4, speed);
 
                </script>";
-    
-    
-}
-?>
+            }
+            ?>
 
         </div>
 
@@ -103,106 +82,106 @@ if (isset($_POST["submit"])) {
     <script src="../resources/js/function.js"></script>
 
     <script>
-    var btn_m = document.getElementById("btn_m");
-    var cursor = document.querySelector(".cursor");
-    var cursorBorder = document.querySelector(".cursor-border");
-    let switch_count = 0;
-    var getXY = function(event, element) {
-        var x = event.clientX;
-        var y = event.clientY;
-        var rect = element.getBoundingClientRect();
-        x -= rect.width / 2;
-        y -= rect.height / 2;
-        return [x, y];
-    };
-    btn_m.addEventListener("click", function() {
-        if (switch_count == 0) {
-            switch_count = 1;
-            Theme();
-        } else {
-            bg_music.pause();
-            switch_count = 0;
+        var btn_m = document.getElementById("btn_m");
+        var cursor = document.querySelector(".cursor");
+        var cursorBorder = document.querySelector(".cursor-border");
+        let switch_count = 0;
+        var getXY = function(event, element) {
+            var x = event.clientX;
+            var y = event.clientY;
+            var rect = element.getBoundingClientRect();
+            x -= rect.width / 2;
+            y -= rect.height / 2;
+            return [x, y];
+        };
+        btn_m.addEventListener("click", function() {
+            if (switch_count == 0) {
+                switch_count = 1;
+                Theme();
+            } else {
+                bg_music.pause();
+                switch_count = 0;
 
-        }
-
-    });
-    document.addEventListener("mouseenter", function(e) {
-        cursor.animate([{
-            opacity: 0
-        }, {
-            opacity: 1
-        }], {
-            duration: 300,
-            fill: "forwards"
-        });
-        cursorBorder.animate([{
-                opacity: 0
-            },
-            {
-                opacity: 0.8
             }
-        ], {
-            duration: 300,
-            fill: "forwards"
-        });
-    });
-    document.addEventListener("mousemove", function(e) {
-        var _a = getXY(e, cursor),
-            cursorX = _a[0],
-            cursorY = _a[1];
-        var _b = getXY(e, cursorBorder),
-            cursorBorderX = _b[0],
-            cursorBorderY = _b[1];
-        var targetName = e.target.tagName;
-        if (targetName === "A" || targetName === "LABEL" || targetName === "BUTTON") {
-            cursorBorder.classList.add("on-focus");
-        } else {
-            cursorBorder.classList.remove("on-focus");
-        }
-        cursor.animate([{
-            transform: "translate(".concat(cursorX, "px, ").concat(cursorY, "px)")
-        }, {
-            transform: "translate(".concat(cursorX, "px, ").concat(cursorY, "px)")
-        }], {
-            duration: 300,
-            fill: "forwards",
-            delay: 50
-        });
-        cursorBorder.animate([{
-            transform: "translate(".concat(cursorBorderX, "px, ").concat(cursorBorderY, "px)")
-        }, {
-            transform: "translate(".concat(cursorBorderX, "px, ").concat(cursorBorderY, "px)")
-        }], {
-            duration: cursorBorder.classList.contains("on-focus") ? 1500 : 300,
-            fill: "forwards",
-            delay: 150
-        });
-    });
-    document.addEventListener("mouseleave", function(e) {
-        cursor.animate([{
-            opacity: 0.8
-        }, {
-            opacity: 0
-        }], {
-            duration: 500,
-            fill: "forwards"
-        });
-        cursorBorder.animate([{
-                opacity: 0.8
-            },
-            {
-                opacity: 0
-            }
-        ], {
-            duration: 500,
-            fill: "forwards"
-        });
-    });
 
-    function Theme() {
-        bg_music.volume = 0.5;
-        bg_music.play();
-    }
+        });
+        document.addEventListener("mouseenter", function(e) {
+            cursor.animate([{
+                opacity: 0
+            }, {
+                opacity: 1
+            }], {
+                duration: 300,
+                fill: "forwards"
+            });
+            cursorBorder.animate([{
+                    opacity: 0
+                },
+                {
+                    opacity: 0.8
+                }
+            ], {
+                duration: 300,
+                fill: "forwards"
+            });
+        });
+        document.addEventListener("mousemove", function(e) {
+            var _a = getXY(e, cursor),
+                cursorX = _a[0],
+                cursorY = _a[1];
+            var _b = getXY(e, cursorBorder),
+                cursorBorderX = _b[0],
+                cursorBorderY = _b[1];
+            var targetName = e.target.tagName;
+            if (targetName === "A" || targetName === "LABEL" || targetName === "BUTTON") {
+                cursorBorder.classList.add("on-focus");
+            } else {
+                cursorBorder.classList.remove("on-focus");
+            }
+            cursor.animate([{
+                transform: "translate(".concat(cursorX, "px, ").concat(cursorY, "px)")
+            }, {
+                transform: "translate(".concat(cursorX, "px, ").concat(cursorY, "px)")
+            }], {
+                duration: 300,
+                fill: "forwards",
+                delay: 50
+            });
+            cursorBorder.animate([{
+                transform: "translate(".concat(cursorBorderX, "px, ").concat(cursorBorderY, "px)")
+            }, {
+                transform: "translate(".concat(cursorBorderX, "px, ").concat(cursorBorderY, "px)")
+            }], {
+                duration: cursorBorder.classList.contains("on-focus") ? 1500 : 300,
+                fill: "forwards",
+                delay: 150
+            });
+        });
+        document.addEventListener("mouseleave", function(e) {
+            cursor.animate([{
+                opacity: 0.8
+            }, {
+                opacity: 0
+            }], {
+                duration: 500,
+                fill: "forwards"
+            });
+            cursorBorder.animate([{
+                    opacity: 0.8
+                },
+                {
+                    opacity: 0
+                }
+            ], {
+                duration: 500,
+                fill: "forwards"
+            });
+        });
+
+        function Theme() {
+            bg_music.volume = 0.5;
+            bg_music.play();
+        }
     </script>
 </body>
 
